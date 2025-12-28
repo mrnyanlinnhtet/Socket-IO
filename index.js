1deab4b90864c23ee36f76e0269e7a5e8df0e517
Mr.Nyan Linn Htet <mr.nyanlinnhtet13@gmail.com>
2025-12-28 20:53:33 +0630

    socket setup

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let express = require("express");
+let socket = require("socket.io");
 
 /**______ App setup  ______*/
 let app = express();
@@ -15,4 +16,11 @@ let server = app.listen(port, () => {
 /**______ Route setup  ______*/
 app.get("/", (res, req) => {
    req.sendFile(__dirname + "/public/index.html");
+});
+
+
+/**______ Socket setup  ______*/
+let io = socket(server);
+io.on("connection", (socket) => {
+    console.log("Socket connection is connected!", socket.id);
 });
